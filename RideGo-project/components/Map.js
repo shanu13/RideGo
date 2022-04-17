d12c43bbef1f9d5a9fe85acd097762a9821b8d6f
shantanu agrawal <shantanua9@gmail.com.com>
2022-04-17 10:47:57 +0530

    changes map

--- RideGo-project/components/Map.js
+++ RideGo-project/components/Map.js
@@ -4,6 +4,7 @@ import { UberContext } from '../context/uberContext'
 
 const style = {
   wrapper: `flex-1 h-full w-full`,
+  
 }
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2h1YmhhbWpoYXdhciIsImEiOiJjbDA2cXJrbW0wMWRzM2JyMGdsZDZuN3hpIn0.D7qs0GFLm3gdUcVXGkujvQ';
@@ -48,15 +49,61 @@ useEffect(() =>{
   })()
 },[])
 
+// useEffect(() => {
+//   const map = new mapboxgl.Map({
+//     container: 'map',
+//     style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
+//     center: [91.55845402235552, 26.080719523125097],
+//     zoom: 12,
+//   });
+
+//   map.addControl(
+//     new mapboxgl.GeolocateControl({
+//     positionOptions: {
+//     enableHighAccuracy: true
+//     },
+//     // When active the map will receive updates to the device's location as it changes.
+//     trackUserLocation: true,
+//     showUserLocation:true,
+//     // Draw an arrow next to the location dot to indicate which direction the device is heading.
+//     showUserHeading: true
+//     })
+//     ,'bottom-right');
+
+//     navigator.geolocation.getCurrentPosition(position => {
+//       const userCoordinates = [position.coords.longitude, position.coords.latitude];
+//       map.flyTo({
+//         center: userCoordinates,
+//         zoom: 12
+//       });
+     
+//     });
+// },[])
 
 
   useEffect(() => {
+
+   
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
       center: [91.55845402235552, 26.080719523125097],
-      zoom: 11,
+      zoom: 12,
     })
+
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+      positionOptions: {
+      enableHighAccuracy: true
+      },
+      // When active the map will receive updates to the device's location as it changes.
+      trackUserLocation: true,
+      showUserLocation:true,
+      // Draw an arrow next to the location dot to indicate which direction the device is heading.
+      showUserHeading: true
+      })
+      ,'bottom-right');
+
 
     if (pickupCoordinates) {
       addToMap(map, pickupCoordinates)
